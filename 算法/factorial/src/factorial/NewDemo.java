aee07191ad32c23f56eb8bd57d017300ba7a6637
loooody <319667916@qq.com>
2018-08-17 10:12:26 +0800

    添加逆序冒泡排序算法实现

--- 算法/factorial/src/factorial/NewDemo.java
+++ 算法/factorial/src/factorial/NewDemo.java
@@ -15,7 +15,37 @@ public class NewDemo {
 		return mul;
 	}
 	
+	public int[] bubbleSort(int[] arr) {
+		if(arr == null || arr.length == 0) {
+			return null;
+		}
+		int j = arr.length - 1;
+		for(int i = 0;i < j;i++) {
+			for(j = arr.length - 1; j>i ;j--) {
+				if(arr[j] < arr[i]) {
+					int temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+		return arr;
+	}
+		
+	
+	
 	public static void main(String[] args) {
-		System.out.println(test(4));
+		System.out.println(test(5));
+		
+		NewDemo demo = new NewDemo();
+		
+		int[] arr = {48,5,7,9,1};
+		
+		int[] arr1 = demo.bubbleSort(arr);
+		
+		for(int i=0;i<arr1.length;i++) {
+			System.out.print(arr1[i] + "  ");
+		}
+		
 	}
 }
