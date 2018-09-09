b5867248118a18116be482b8d3d4f31264f8a72c
loooody <319667916@qq.com>
2018-09-09 20:30:57 +0800

    统计给定字符在文件中出现的次数

--- 2018/9月/fileCopy/src/com/fileCopy.java
+++ 2018/9月/fileCopy/src/com/fileCopy.java
@@ -1,9 +1,11 @@
 package com;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -14,9 +16,17 @@ public class fileCopy {
 		File source = new File("D://file/file.txt");
 		File target = new File("D://file/copy.txt");
 		
-		copy(source,target);
+	//	copy(source,target);
+		
+		System.out.println(countWordInFile(source,"new"));
 	}
 	
+	/**
+	 * 文件拷贝
+	 * @param source
+	 * @param target
+	 * @throws IOException
+	 */
 	public static void copy(File source,File target) throws IOException {
 		InputStream in = new FileInputStream(source);
 		OutputStream out = new FileOutputStream(target);
@@ -26,5 +36,24 @@ public class fileCopy {
 			out.write(buffer, 0, bytesToRead);
 		}
 		System.out.println("复制完成");
+	}
+	
+	/**
+	 * 统计给定字符串在文件中出现的次数
+	 * @throws IOException 
+	 */
+	public static int countWordInFile(File filename,String word) throws IOException {
+		int count = 0;
+		FileReader fr = new FileReader(filename);
+		BufferedReader br = new BufferedReader(fr);
+		String line = null;
+		while((line = br.readLine()) != null) {
+			int index = -1;
+			while(line.length() > word.length() && (index = line.indexOf(word)) >= 0) {
+				count++;
+				line = line.substring(index + word.length());
+			}
+		}
+		return count;
 	}
 }
