856b7dc2e9692a35920b898d30553895e2469fbd
Lvxiaohui <319667916@qq.com>
2018-09-13 23:17:16 +0800

    第一个错误的版本

---
+++ 算法/leetcode/al_9_13_1.java
@@ -0,0 +1,12 @@
+public int firstBadVersion(int n){
+ int min = 1, max = n, mid = 0;
+ while(min <= max){
+   mid = min + (max + min) / 2;
+   if(isBadVersion(mid)){
+     max = mid -1;
+   }else{
+     min = mid + 1;
+   }
+ }
+  return min;
+}
