cc5edd4b4182aaba52b639a70f41273087ce87fb
Lvxiaohui <319667916@qq.com>
2018-10-15 21:07:36 +0800

    65.不用加减乘除做加法

---
+++ 算法/剑指offer/Add.java
@@ -0,0 +1,21 @@
+package date1015;
+
+public class Add {
+
+    public static int Add(int num1, int num2) {
+        int sum, carry;
+        do {
+            sum = num1 ^ num2;
+            carry = (num1 & num2) << 1;
+            num1 = sum;
+            num2 = carry;
+        } while (num2 != 0);
+
+        return num1;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Add(3, 9));
+    }
+}
